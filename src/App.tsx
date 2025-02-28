b7c4fb363645164e23efc2f3280467367623559f
Aznim Rahman <aznimr@protonmail.com>
2025-02-28 15:07:08 +0000

    add loading spinner component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
+import { LoadingSpinner } from "@/components/loading-spinner";
 import { QuizCard } from "@/components/quiz-card";
+import { TQuizData } from "@/lib/data";
+import { useState } from "react";
 
 function App() {
+  const [currentQuiz, setCurrentQuiz] = useState<TQuizData | null>(null);
+  const [loading, setLoading] = useState(true);
   return (
     <div className="grid place-items-center p-6 font-poppins">
       <h1>Seneca Interview Assessment</h1>
-      <QuizCard />
+
+      {loading ? (
+        <div className="flex justify-center items-center h-svh">
+          <LoadingSpinner />
+        </div>
+      ) : currentQuiz ? (
+        <QuizCard />
+      ) : (
+        <div></div>
+      )}
     </div>
   );
 }
