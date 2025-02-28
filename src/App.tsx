ff2e2eefdbb11f2604d7b1f3a922bc466317a49d
Aznim Rahman <aznimr@protonmail.com>
2025-02-28 22:22:29 +0000

    improving the selection logic and update data file

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,42 @@
 import { LoadingSpinner } from "@/components/loading-spinner";
 import { QuizCard } from "@/components/quiz-card";
-import { TQuizData } from "@/lib/data";
-import { useState } from "react";
+import { quizData, TQuizData } from "@/lib/data";
+import { useEffect, useState } from "react";
 
 function App() {
   const [currentQuiz, setCurrentQuiz] = useState<TQuizData | null>(null);
   const [loading, setLoading] = useState(true);
-  return (
-    <div className="grid place-items-center p-6 font-poppins">
-      <h1>Seneca Interview Assessment</h1>
+  // const [endQuiz, setEndQuiz] = useState(false);
 
-      {loading ? (
-        <div className="flex justify-center items-center h-svh">
-          <LoadingSpinner />
-        </div>
-      ) : currentQuiz ? (
-        <QuizCard />
-      ) : (
-        <div></div>
-      )}
+  useEffect(() => {
+    // Pretend to request API here with setTimeout to simulate network delay
+    const fetchData = async () => {
+      await new Promise((resolve) => setTimeout(resolve, 800));
+      // Set the quiz to the first item the array
+      const fetchedQuiz = quizData[0];
+      setCurrentQuiz(fetchedQuiz);
+      setLoading(false);
+    };
+    fetchData();
+    // const fetchedQuiz = quizData[0];
+    // setCurrentQuiz(fetchedQuiz);
+  }, []);
+
+  return (
+    <div className="font-poppins font-medium">
+      <h1 className="text-center my-10">Seneca Interview Assessment</h1>
+      <div className="flex justify-center items-center">
+        {/* {currentQuiz && <QuizCard quizData={currentQuiz} />} */}
+        {loading ? (
+          <div className="h-svh">
+            <LoadingSpinner />
+          </div>
+        ) : currentQuiz ? (
+          <QuizCard quizData={currentQuiz} />
+        ) : (
+          <p>There is no more quizzes to load.</p>
+        )}
+      </div>
     </div>
   );
 }
--- src/components/quiz-card.tsx
+++ src/components/quiz-card.tsx
@@ -1,3 +1,57 @@
-export function QuizCard() {
-  return <div></div>;
+import { TQuizData } from "@/lib/data";
+import { useState } from "react";
+
+type TQuizCardProps = {
+  quizData: TQuizData;
+};
+
+export function QuizCard({ quizData }: TQuizCardProps) {
+  const getRandomInitialSelections = () => {
+    const initialSelections: Record<number, string | null> = {};
+    // code here
+    return initialSelections;
+  };
+  const [selectedAnswers, setSelectedAnswers] = useState<
+    Record<number, string | null>
+  >(getRandomInitialSelections());
+  const [isAllCorrect, setIsAllCorrect] = useState(false);
+  const [answerMessage, setAnswerMessage] = useState<string | null>(null);
+
+  const handleToggleChange = (selectionId: number, selectedOption: string) => {
+    console.log("selection", selectedOption);
+    setSelectedAnswers((prev) => ({ ...prev, [selectionId]: selectedOption }));
+  };
+
+  return (
+    <div className="rounded-lg shadow-lg transition-colors duration-300 bg-gray-300 w-[90%] mx-auto py-10 px-6 h-full text-white">
+      <form className="flex flex-col items-center justify-center gap-10">
+        <h2 className="text-lg">{quizData.question}</h2>
+        <div className="flex flex-col gap-3">
+          {quizData.selections.map((selection) => (
+            <div
+              key={selection.id}
+              className="font-normal border-[2px] flex flex-col gap-3 rounded-full max-lg:border-none border-white border-opacity-75 lg:gap-0 lg:flex-row lg:min-w-[850px]"
+            >
+              {selection.options.map((option) => (
+                <button
+                  key={option}
+                  type="button"
+                  onClick={() => {
+                    handleToggleChange(selection.id, option);
+                  }}
+                  className={` hover:text-gray-100 p-4 rounded-full text-center transition-colors duration-300 flex-1 min-w-fit lg:w-full border-[2px] border-white border-opacity-75 lg:border-none
+                  
+                  `}
+                >
+                  {option}
+                </button>
+              ))}
+            </div>
+          ))}
+        </div>
+
+        {answerMessage && <p>{answerMessage}</p>}
+      </form>
+    </div>
+  );
 }
