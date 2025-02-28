b14afcdced64557f6912b9d2001bc641729a354d
Aznim Rahman <aznimr@protonmail.com>
2025-02-28 14:41:51 +0000

    install node

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import { QuizCard } from "@/components/quiz-card";
+
 function App() {
   return (
     <div className="grid place-items-center p-6 font-poppins">
       <h1>Seneca Interview Assessment</h1>
-      <div className="border-2">hello</div>
+      <QuizCard />
     </div>
   );
 }
