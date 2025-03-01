00158f0065fe2fa960d6f2a5f78d93dd3ae91c92
Aznim Rahman <aznimr@protonmail.com>
2025-03-01 14:24:24 +0000

    cleanup index.ts

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,7 +1,3 @@
-// import clsx, { ClassValue } from "clsx";
-// import { twMerge } from "tailwind-merge";
-
-// export const cn = (...classes: ClassValue[]) => twMerge(clsx(...classes));
 import classNames from "classnames";
 
 export const cn = (...classes: (string | undefined)[]) => {
